5b9c24d13747091f0b656a7357b9707853562e27
kapicaluk <kapicaluk@gmail.com>
2017-10-30 16:16:14 -0400

    server setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// const db = require('./db');
+
+const PORT = process.env.PORT || 8888; 
+
 const app = express();
 
-// you'll of course want static middleware so your browser can request things like your 'bundle.js'
-app.use(express.static(path.join(__dirname, './public')))
+const server = app.listen(PORT, ()=> console.log(`Server listening on ${PORT}`))
 
-// Any routes or other various middlewares should go here!
+module.exports = app;
 
-// Make sure this is right at the end of your server logic!
-// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
-// (However, if you have middleware to serve up 404s, that go would before this as well)
-app.get('*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, './public'));
-});
+// db.sync().then(()=> console.log('Database party on! '))
+
+//logginig middleware
+app.use(morgan('dev'));
+
+//static middleware
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+
+//body parser middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+// API routes 
+
+// app.use('/api', require('./api'));
+
+//404 middleware
+app.use((req,res,next) => {
+	path.extname(req.path).length > 0 ? 
+	res.status(404).send('Not found') : next()
+})
+
+app.use('*', (req,res,next) => res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+	);
+
+//error handling endware 
+app.use((err,req,res,next) => res.status(err.status || 500).send(err.message || 'Internal sercer error :(')
+	);
